68343155e397a8948a876ae1c67a37c3bf61db9a
scottdmccarter <44345683+scottdmccarter@users.noreply.github.com>
2019-01-22 22:42:23 +0000

    initial commit

---
+++ related/hammingGeneratorMatrix(1).py
@@ -0,0 +1,49 @@
+#function HammingG
+#input: a number r
+#output: G, the generator matrix of the (2^r-1,2^r-r-1) Hamming code
+def hammingGeneratorMatrix(r):
+    n = 2**r-1
+    
+    #construct permutation pi
+    pi = []
+    for i in range(r):
+        pi.append(2**(r-i-1))
+    for j in range(1,r):
+        for k in range(2**j+1,2**(j+1)):
+            pi.append(k)
+
+    #construct rho = pi^(-1)
+    rho = []
+    for i in range(n):
+        rho.append(pi.index(i+1))
+
+    #construct H'
+    H = []
+    for i in range(r,n):
+        H.append(decimalToVector(pi[i],r))
+
+    #construct G'
+    GG = [list(i) for i in zip(*H)]
+    for i in range(n-r):
+        GG.append(decimalToVector(2**(n-r-i-1),n-r))
+
+    #apply rho to get Gtranpose
+    G = []
+    for i in range(n):
+        G.append(GG[rho[i]])
+
+    #transpose    
+    G = [list(i) for i in zip(*G)]
+
+    return G
+
+
+#function decimalToVector
+#input: numbers n and r (0 <= n<2**r)
+#output: a string v of r bits representing n
+def decimalToVector(n,r): 
+    v = []
+    for s in range(r):
+        v.insert(0,n%2)
+        n //= 2
+    return v
